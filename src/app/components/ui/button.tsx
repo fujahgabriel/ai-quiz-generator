311692f510ab2546b1b1e5f793c65b8f74635cc0
Gabriel Ajenifuja <gabrielfuja001@gmail.com>
2024-10-05 19:07:04 +0100

    Update button.tsx

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -14,7 +14,7 @@ const Button: React.FC<ButtonProps> = ({ children, onClick, disabled, type = 'bu
             type={type}
             disabled={disabled}
             onClick={onClick}
-            className={`px-4 py-2 bg-slate-500 text-white rounded hover:bg-slate-600 active:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-opacity-75 ${className}`}
+            className={`${className} px-4 py-2 bg-slate-500 text-white rounded hover:bg-slate-600 active:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-opacity-75 `}
         >
             {children}
         </button>
